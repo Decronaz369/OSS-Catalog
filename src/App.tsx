b31a980f4f47bda75e79c567a0f7a08eb4eefea5
Decronaz <decronaz369@gmail.com>
2025-07-30 11:08:47 +0700

    35

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import HomePage from "./assets/Navigator";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <HomePage />
+      <Analytics />
     </>
   );
 }
